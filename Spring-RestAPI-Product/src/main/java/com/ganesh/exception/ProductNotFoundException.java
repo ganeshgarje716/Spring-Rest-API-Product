b50646169cc74ee2614ebc5a7c7bd51249782d00
Ganesh <ganeshgarje716@gmail.com>
2026-06-23 20:51:26 +0530

    Create ProductNotFoundException Class

---
+++ Spring-RestAPI-Product/src/main/java/com/ganesh/exception/ProductNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ganesh.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+	
+	public ProductNotFoundException(String msg) {
+		
+		super(msg); 
+		// TODO Auto-generated constructor stub
+	}
+
+}
